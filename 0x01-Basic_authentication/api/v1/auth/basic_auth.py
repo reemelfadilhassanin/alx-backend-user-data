a1b2afafd183129f5109f4d5b67d010f8c28bd2b
root <root@Reem.localdomain>
2024-11-12 18:21:52 +0300

    bascic email and passw

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,7 +15,7 @@ class BasicAuth(Auth):
     """
 
     def decode_base64_authorization_header(self,
-                                           base64_authorizat: str) -> str:
+                                           base64_authorization: str) -> str:
         """Decodes a base64-encoded authorization header.
 
         Args:
@@ -25,14 +25,35 @@ class BasicAuth(Auth):
             str: The decoded value as a UTF-8 string if valid, None otherwise.
         """
         # Check if base64_authorization_header is None or not a string
-        if not isinstance(base64_authorizat, str):
+        if not isinstance(base64_authorization, str):
             return None
 
         # Try to decode the Base64 string
         try:
-            decoded_bytes = base64.b64decode(base64_authorizat,
+            decoded_bytes = base64.b64decode(base64_authorization,
                                              validate=True)
             return decoded_bytes.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             # If decoding fails, return None
             return None
+
+    def extract_user_credentials(self, decoded_base64_: str) -> (str, str):
+        """Extracts user credentials (email and password)
+
+        Args:
+            decoded_base64_authorization_header (str)
+
+        Returns:
+            tuple: A tuple containing the email and password
+        """
+        # Check if decoded_base64_authorization_header is None or not a string
+        if not isinstance(decoded_base64_, str):
+            return None, None
+
+        # Check if the string contains ':'
+        if ':' not in decoded_base64_:
+            return None, None
+
+        # Split the string into email and password
+        email, password = decoded_base64_.split(':', 1)
+        return email, password
