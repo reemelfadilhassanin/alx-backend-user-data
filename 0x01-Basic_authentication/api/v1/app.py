cd4a031d566bf67c7fbde814ab466536fbe01dde
root <root@Reem.localdomain>
2024-11-12 16:51:50 +0300

    error unautherize

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,17 +12,19 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Error handler for 404
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+
 # Error handler for 401 Unauthorized
 @app.errorhandler(401)
 def unauthorized(error):
-    """Unauthorized error handler"""
+    """ Unauthorized error handler """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
