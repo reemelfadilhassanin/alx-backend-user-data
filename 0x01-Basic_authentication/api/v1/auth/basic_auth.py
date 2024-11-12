931631de510e172d3a65238029909e62d228a6a7
root <root@Reem.localdomain>
2024-11-12 17:47:48 +0300

    bascic autheriz

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,28 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Authentication class. Inherit from Auth class.
-        Currently, it doesn't implement any authentication logic yet.
+        This class will handle Basic Auth-related tasks
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the base64 part of the Authorization header for Basic Auth.
+
+        Args:
+            authorization_header (str): The full Authorization header.
+
+        Returns:
+            str: The base64 part after 'Basic'
+        """
+        # Check if authorization_header is None or not a string
+        if authorization_header is None or
+        not isinstance(authorization_header, str):
+            return None
+
+        # Check if the string starts with 'Basic '
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # Return the base64 part (after 'Basic ')
+        return authorization_header[6:]
