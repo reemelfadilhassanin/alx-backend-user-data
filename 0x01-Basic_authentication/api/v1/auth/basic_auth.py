98fdfb3460493f4e708427ed3d209b15140e5ade
root <root@Reem.localdomain>
2024-11-12 17:37:05 +0300

    bascic autheriz

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" BasicAuth class to manage basic authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class. Inherit from Auth class.
+        Currently, it doesn't implement any authentication logic yet.
+    """
+    pass
