76507bb104c3fe77ab5c4c8b40532b0ad2a04194
root <root@Reem.localdomain>
2024-11-14 09:30:34 +0300

    fix pycodestyle

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -130,5 +130,5 @@ def get_current_user() -> str:
       - 404 if user is not authenticated
     """
     if request.current_user is None:
-        abort(404)  # or return 401 Unauthorized depending on the desired behavior
+        abort(404)
     return jsonify(request.current_user.to_json())
